49a2a1fe3576954ab85be19b2883d24c74d83f1a
CarlosCastells <cjcastells@gmail.com>
2015-08-18 20:01:25 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,7 +22,7 @@ exports.index = function(req, res) {
   } else 
   {
     models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes});
+      res.render('quizes/index.ejs', {quizes: quizes, errors: [] });
     }
    ).catch(function(error) { next(error);})
   } 
@@ -30,7 +30,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req,res){
-		res.render('quizes/show', { quiz: req.quiz});
+		res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/	answer
@@ -39,7 +39,7 @@ exports.answer = function(req,res){
 	if (req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';	
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -48,15 +48,24 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-  // save: guarda en DB campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then( function(){ 
-    res.redirect('/quizes'); 
-      })      // res.redirect: Redirección HTTP a lista de preguntas
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,15 @@
 // Definición del modelo de Quiz
 
 module.exports = function(sequilize, DataTypes) {
-	return sequilize.define('Quiz',
-		{ pregunta: DataTypes.STRING,
-		  respuesta: DataTypes.STRING,
-		});
+	return sequilize.define(
+		'Quiz',
+		{ pregunta: {
+        	type: DataTypes.STRING,
+        	validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+      	},
+        respuesta: {
+     	   type: DataTypes.STRING,
+        	validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+    	}
+	});
 }
